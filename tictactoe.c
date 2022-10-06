1b9b79736f45ff429b336a4d6f34730b1e2e58c3
JamesSCrank <jscrank@cpp.edu>
2022-10-05 19:30:34 -0700

    Initial comments proposing how to create program

---
+++ tictactoe.c
@@ -0,0 +1,27 @@
+//Tic tac toe game between 2 players or player vs computer 
+
+int main(){
+    //prompt user for game they should play 
+    cout << "Would you like to play a game of Tic-tac-toe?" << endl; 
+    
+    //ask the user if they would like to play, and check for Yes or No 
+    //if no, give a "restarting program" message 
+    //If yes, "Excellent, which would you like to play?"
+    //give menu options, 1 - Player vs. Player, 2 - Player vs. computer 
+    //if "Player vs. player", the program needs to give prompts for each player to input their choice 
+    //if a tie, system should say so and end the game. 
+    //if a win occurs, system should stop and announce a winner 
+    //Should the player choose "Player vs. Computer"
+    //the program should give space for the player to choose a X or O
+    //while generating a random for the computers turn 
+    //Need to create the visual representation of the grid and each time the player input a number/choice, the system 
+    //needs to be able to find the space in the representation and update it to reflect the new choice  
+    //read information from the console 
+
+    //
+
+
+
+
+    return 0; 
+}
